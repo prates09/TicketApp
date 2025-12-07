8c396afbc6800e5755ea65cde6f103b14eec7537
Pratyush <157958382+prates09@users.noreply.github.com>
2025-12-07 23:52:30 +0530

    Delete TicketApp directory

--- TicketApp/ticket-client/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { Tickets } from './Components/tickets/tickets';
-
-export const routes: Routes = [
-  { path: 'components/tickets', component: Tickets },
-  { path: '', redirectTo: '/components/tickets', pathMatch: 'full' }
-];
